853d8d4d84e8426ef0b4c5691f6452e552ec5646
Kartheek V V S <kartheek.vempati@stash.digiplug.com>
2015-06-22 11:27:50 +0530

    My Change

--- SourceTreeIntroConsoleApp/SourceTreeIntroConsoleApp/Program.cs
+++ SourceTreeIntroConsoleApp/SourceTreeIntroConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace SourceTreeIntroConsoleApp
         {
             // Keep the console window open in debug mode.
             Console.WriteLine("Welcome to SourceTree!");
+	    Console.WriteLine("Welcome Karthika!");
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
